f1c0595154c53e6fa45a2755fd1f015427a5fff0
NikkoAce <moreno.nikkaoce@gmail.com>
2025-08-27 08:38:21 +0800

    fix view requisition

--- backend/routes/stockItemRoutes.js
+++ backend/routes/stockItemRoutes.js
@@ -12,12 +12,12 @@ const { protect, checkPermission } = require('../middlewares/authMiddleware.js')
 
 // @route   GET & POST /api/stock-items
 router.route('/')
-    .get(protect, checkPermission('stock:read'), getAllStockItems)
+    .get(protect, getAllStockItems) // Any authenticated user can view the list of items
     .post(protect, checkPermission('stock:manage'), createStockItem);
 
 // @route   GET, PUT, DELETE /api/stock-items/:id
 router.route('/:id')
-    .get(protect, checkPermission('stock:read'), getStockItemById)
+    .get(protect, getStockItemById) // Any authenticated user can view a single item
     .put(protect, checkPermission('stock:manage'), updateStockItem)
     .delete(protect, checkPermission('stock:manage'), deleteStockItem);
 
