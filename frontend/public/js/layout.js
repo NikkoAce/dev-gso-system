e2326607f572abf40d1bd67b71b8d4def629b900
NikkoAce <moreno.nikkaoce@gmail.com>
2025-09-14 00:48:51 +0800

    fix

--- frontend/public/js/layout.js
+++ frontend/public/js/layout.js
@@ -148,13 +148,7 @@ function getSidebarHTML(user) {
         </select>
       </div>
     </div>
-    <!-- Collapse Button Area -->
-    <div id="sidebar-toggle-container" class="p-2 border-t border-base-300 hidden md:block">
-        <button id="sidebar-toggle-btn" class="btn btn-ghost btn-sm w-full justify-start text-base-content/70 hover:bg-base-300 hover:text-base-content" title="Collapse/Expand Sidebar">
-            <i data-lucide="chevrons-left"></i>
-            <span class="sidebar-text ml-4">Collapse</span>
-        </button>
-    </div>
+    <!-- Toggle button is now in the header -->
   `;
 }
 
