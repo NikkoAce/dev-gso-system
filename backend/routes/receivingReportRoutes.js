61fa37f02d6d6487aab6e9fd14c75b84934cd304
NikkoAce <moreno.nikkaoce@gmail.com>
2025-09-18 21:54:01 +0800

    fix gso-requisition

--- backend/routes/receivingReportRoutes.js
+++ backend/routes/receivingReportRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { createReceivingReport, getReceivingReports } = require('../controllers/receivingReportController');
-const { protect, hasPermission } = require('../middleware/authMiddleware');
+const { protect, checkPermission } = require('../middlewares/authMiddleware');
+const PERMISSIONS = require('../config/permissions');
 
 router.route('/')
-    .post(protect, hasPermission('stock:manage'), createReceivingReport)
-    .get(protect, hasPermission('stock:manage'), getReceivingReports);
+    .post(protect, checkPermission(PERMISSIONS.STOCK_MANAGE), createReceivingReport)
+    .get(protect, checkPermission(PERMISSIONS.STOCK_MANAGE), getReceivingReports);
 
 module.exports = router;
