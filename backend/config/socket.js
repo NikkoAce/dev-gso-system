da42e7131d55c1853a3381e736bc66b86c3f1203
NikkoAce <moreno.nikkaoce@gmail.com>
2025-09-21 20:45:04 +0800

    fix

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -2,6 +2,9 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 let io;
+// NEW: A map to track which user is in which room.
+// Key: socket.id, Value: { user: { name, office }, room: 'office:...' }
+const connectedUsers = new Map();
 
 /**
  * Initializes the Socket.IO server instance and sets up event handlers.
@@ -36,18 +39,47 @@ const initSocket = (socketIoInstance) => {
     io.on('connection', (socket) => {
         console.log(`User connected: ${socket.user.name} (${socket.id})`);
 
-        socket.on('join-room', (room) => {
-            socket.join(room);
-            console.log(`Socket ${socket.id} joined room: ${room}`);
+        socket.on('join-room', (roomName) => {
+            const previousRoom = connectedUsers.get(socket.id)?.room;
+
+            // If the user was in a different room, make them leave it first.
+            if (previousRoom && previousRoom !== roomName) {
+                socket.leave(previousRoom);
+                // Notify others in the old room that this user has left.
+                socket.to(previousRoom).emit('user-left', { name: socket.user.name, id: socket.id });
+                console.log(`Socket ${socket.id} left room: ${previousRoom}`);
+            }
+
+            // Join the new room.
+            socket.join(roomName);
+            // Track the user's current room.
+            connectedUsers.set(socket.id, { user: socket.user, room: roomName });
+            console.log(`Socket ${socket.id} joined room: ${roomName}`);
+
+            // Notify others in the new room that this user has joined.
+            socket.to(roomName).emit('user-joined', { name: socket.user.name, id: socket.id });
         });
 
         socket.on('leave-room', (room) => {
             socket.leave(room);
+            // Also update our tracking map
+            const userInfo = connectedUsers.get(socket.id);
+            if (userInfo && userInfo.room === room) {
+                socket.to(userInfo.room).emit('user-left', { name: socket.user.name, id: socket.id });
+                connectedUsers.delete(socket.id);
+            }
             console.log(`Socket ${socket.id} left room: ${room}`);
         });
 
         socket.on('disconnect', () => {
-            console.log(`User disconnected: ${socket.id}`);
+            const userInfo = connectedUsers.get(socket.id);
+            if (userInfo && userInfo.room) {
+                // Notify others in the room that this user has left.
+                socket.to(userInfo.room).emit('user-left', { name: socket.user.name, id: socket.id });
+                // Clean up the tracking map.
+                connectedUsers.delete(socket.id);
+            }
+            console.log(`User disconnected: ${socket.user.name} (${socket.id})`);
         });
     });
 };
