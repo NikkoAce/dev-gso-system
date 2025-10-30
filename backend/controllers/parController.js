0e30c107ff85657e51d1b2a7b9edc81e5dcebc7b
NikkoAce <moreno.nikkaoce@gmail.com>
2025-10-30 16:47:18 +0800

    fix

--- backend/controllers/parController.js
+++ backend/controllers/parController.js
@@ -14,8 +14,8 @@ const createPAR = async (req, res) => {
     const alreadyAssigned = await Asset.find({
         _id: { $in: assetObjectIds },
         $or: [
-            { assignedPAR: { $ne: null, $ne: '' } },
-            { assignedICS: { $ne: null, $ne: '' } }
+            { assignedPAR: { $exists: true, $ne: null, $ne: '' } },
+            { assignedICS: { $exists: true, $ne: null, $ne: '' } }
         ]
     }).select('propertyNumber assignedPAR assignedICS');
 
